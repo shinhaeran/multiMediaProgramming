3c71ec84b414a66f9d6f9c36d3cc0507ff38da5f
shinhaeran <40760704+shinhaeran@users.noreply.github.com>
2019-03-27 15:05:41 +0900

    핀홀 카메라
    뭔지는잘 몰겠네옹

--- 소스.cpp
+++ 소스.cpp
@@ -259,173 +259,13 @@ void class_0313() {
 
 	Affine_Transform(image, Affine_image, height, width, a, b, c, d,0,0);
 
-
 }
-
-
-//
-//
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <math.h>
-//#include <malloc.h>
-//
-//#include <opencv2/opencv.hpp>   
-//#include <opencv2/core/core.hpp>   
-//#include <opencv2/highgui/highgui.hpp>  
-//
-//using namespace cv;
-//
-//#define PI 3.14159265359
-//
-//typedef struct {
-//	int r, g, b;
-//}int_rgb;
-//
-//
-//int** IntAlloc2(int height, int width)
-//{
-//	int** tmp;
-//	tmp = (int**)calloc(height, sizeof(int*));
-//	for (int i = 0; i<height; i++)
-//		tmp[i] = (int*)calloc(width, sizeof(int));
-//	return(tmp);
-//}
-//
-//void IntFree2(int** image, int height, int width)
-//{
-//	for (int i = 0; i<height; i++)
-//		free(image[i]);
-//
-//	free(image);
-//}
-//
-//int_rgb** IntColorAlloc2(int height, int width)
-//{
-//	int_rgb** tmp;
-//	tmp = (int_rgb**)calloc(height, sizeof(int_rgb*));
-//	for (int i = 0; i<height; i++)
-//		tmp[i] = (int_rgb*)calloc(width, sizeof(int_rgb));
-//	return(tmp);
-//}
-//
-//void IntColorFree2(int_rgb** image, int height, int width)
-//{
-//	for (int i = 0; i<height; i++)
-//		free(image[i]);
-//
-//	free(image);
-//}
-//
-//int** ReadImage(char* name, int* height, int* width)
-//{
-//	Mat img = imread(name, IMREAD_GRAYSCALE);
-//	int** image = (int**)IntAlloc2(img.cols, img.rows);
-//
-//	*width = img.cols;
-//	*height = img.rows;
-//
-//	for (int i = 0; i<img.rows; i++)
-//		for (int j = 0; j<img.cols; j++)
-//			image[i][j] = img.at<unsigned char>(i, j);
-//
-//	return(image);
-//}
-//
-//void WriteImage(char* name, int** image, int height, int width)
-//{
-//	Mat img(height, width, CV_8UC1);
-//	for (int i = 0; i<height; i++)
-//		for (int j = 0; j<width; j++)
-//			img.at<unsigned char>(i, j) = (unsigned char)image[i][j];
-//
-//	imwrite(name, img);
-//}
-//
-//
-//void ImageShow(char* winname, int** image, int height, int width)
-//{
-//	Mat img(height, width, CV_8UC1);
-//	for (int i = 0; i<height; i++)
-//		for (int j = 0; j<width; j++)
-//			img.at<unsigned char>(i, j) = (unsigned char)image[i][j];
-//	imshow(winname, img);
-//	waitKey(0);
-//}
-//
-//
-//
-//int_rgb** ReadColorImage(char* name, int* height, int* width)
-//{
-//	Mat img = imread(name, IMREAD_COLOR);
-//	int_rgb** image = (int_rgb**)IntColorAlloc2(img.cols, img.rows);
-//
-//	*width = img.cols;
-//	*height = img.rows;
-//
-//	for (int i = 0; i<img.rows; i++)
-//		for (int j = 0; j < img.cols; j++) {
-//			image[i][j].b = img.at<Vec3b>(i, j)[0];
-//			image[i][j].g = img.at<Vec3b>(i, j)[1];
-//			image[i][j].r = img.at<Vec3b>(i, j)[2];
-//		}
-//
-//	return(image);
-//}
-//
-//void WriteColorImage(char* name, int_rgb** image, int height, int width)
-//{
-//	Mat img(height, width, CV_8UC3);
-//	for (int i = 0; i<height; i++)
-//		for (int j = 0; j < width; j++) {
-//			img.at<Vec3b>(i, j)[0] = (unsigned char)image[i][j].b;
-//			img.at<Vec3b>(i, j)[1] = (unsigned char)image[i][j].g;
-//			img.at<Vec3b>(i, j)[2] = (unsigned char)image[i][j].r;
-//		}
-//
-//	imwrite(name, img);
-//}
-//
-//void ColorImageShow(char* winname, int_rgb** image, int height, int width)
-//{
-//	Mat img(height, width, CV_8UC3);
-//	for (int i = 0; i<height; i++)
-//		for (int j = 0; j<width; j++) {
-//			img.at<Vec3b>(i, j)[0] = (unsigned char)image[i][j].b;
-//			img.at<Vec3b>(i, j)[1] = (unsigned char)image[i][j].g;
-//			img.at<Vec3b>(i, j)[2] = (unsigned char)image[i][j].r;
-//		}
-//	imshow(winname, img);
-//
-//}
-//
-//template <typename _TP>
-//void ConnectedComponentLabeling(_TP** seg, int height, int width, int** label, int* no_label)
-//{
-//
-//	//Mat bw = threshval < 128 ? (img < threshval) : (img > threshval);
-//	Mat bw(height, width, CV_8U);
-//
-//	for (int i = 0; i < height; i++) {
-//		for (int j = 0; j < width; j++)
-//			bw.at<unsigned char>(i, j) = (unsigned char)seg[i][j];
-//	}
-//	Mat labelImage(bw.size(), CV_32S);
-//	*no_label = connectedComponents(bw, labelImage, 8); // 0까지 포함된 갯수임
-//
-//	(*no_label)--;
-//
-//	for (int i = 0; i < height; i++) {
-//		for (int j = 0; j < width; j++)
-//			label[i][j] = labelImage.at<int>(i, j);
-//	}
-//}
 
 #define imax(x, y) ((x)>(y) ? x : y)
 #define imin(x, y) ((x)<(y) ? x : y)
 
 int BilinearInterpolation(int** image, int width, int height, double x, double y) // 선형보간
-{
+{	
 	int x_int = (int)x;
 	int y_int = (int)y;
 
@@ -449,8 +289,8 @@ void upgrade_Affine(int** image, int** img_out, int height, int width, float a, 
 	
 	for (int y_prime = 0; y_prime < height; y_prime++) {
 		for (int x_prime = 0; x_prime < width; x_prime++) {
-			double x = s*(d*x_prime - d*t1 - b*y_prime +b*t2);
-			double y = s*(-c*x_prime + c*t1 + a*y_prime - a*t2);0.
+			double x = s*(d*x_prime  - d*t1 - b*y_prime + b*t2) + width/2;
+			double y = s*(-c*x_prime + c*t1 + a*y_prime - a*t2) + height/2;
 			if(x< (double)width && x>=0.0 && y< (double)height && y>=0.0)
 				img_out[y_prime][x_prime] = BilinearInterpolation(image, width, height, x, y);
 		}
@@ -460,17 +300,80 @@ void upgrade_Affine(int** image, int** img_out, int height, int width, float a, 
 	ImageShow("test2", img_out, height, width);
 
 }
-int main() {
+
+void class0320() {
 	int height, width;
+
 	int** image = ReadImage("Koala.jpg", &height, &width);
 	
 	int** image_out = IntAlloc2(height, width);
+	
 	float a, b, c, d;
-	a = 0.3; b = 1.2; c = 0; d = 2;
+	a = 1 / sqrt(2) ; b = 1 ; c = -1 ; d = 1 / sqrt(2) ;
+	//a = -1; b = 0; c = 0; d = 1;
 
-	upgrade_Affine(image, image_out, height, width, a, b, c, d, 0, 1);
-
+	upgrade_Affine(image, image_out, height, width, a, b, c, d, width/2, height/2);
 
 	//ImageShow("1", image, height, width);
+}
 
+typedef struct POS3D{
+	float x;
+	float y;
+	float z;
+};
+
+void init_POS3D(POS3D* P, float x, float y, float z) {
+	P->x = x;
+	P->y = y;
+	P->z = z;
+}
+
+
+void drawLine_pinhole(int** image, int** image_out, int height, int width, POS3D Px0, POS3D Px1 ) {
+	float X, Y, Z;
+	float x, y, z;
+	float f1 = 200; float f2 = 300;
+	float px = width / 2; float py = height / 2; float w = 0;
+
+	//l = p+t(q-p) 200,300
+
+	for (float t = 0.1; t < 100; t =t+0.1) {
+		X = Px0.x + t*(Px1.x - Px0.x);
+		Y = Px0.y + t*(Px1.y - Px0.y);
+		Z = Px0.z + t*(Px1.z - Px0.z);
+
+		x = f1*X + px*Z;
+		y = f2*Y + py*Z;
+		w = Z;
+		
+		image_out[int((y / w) + 0.5)][int((x / w) + 0.5)] = 255;
+		printf("%f %f %f \n", x/w,y/w,w);
+	}
+
+	ImageShow("aa", image_out, height, width);
+
+
+
+}
+
+/*
+ 핀홀 카메라: (4,2)가 어느 점에 매칭되는지 알고 싶으면 뒤에 숫자 2 로 나누면 (2,1) <- 
+ 3차원의 경우: (4,6,2)->(2,3,1) ; 3차원의 공간의 그림자: 2차원 ; projective space
+*/
+int main() {
+	int height, width;
+
+	int** image = ReadImage("Koala.jpg", &height, &width);
+	int** image_out = IntAlloc2(height, width);
+
+	ImageShow("qq", image, height, width);
+
+	POS3D Px0, Px1;
+
+	init_POS3D(&Px0, 1, 2, 3);
+	init_POS3D(&Px1, 5, 6, 7);
+
+	drawLine_pinhole(image, image_out, height, width, Px0,Px1);
+	
 }
